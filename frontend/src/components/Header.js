6f474f0011b33b04dd7526b786d63d2f2a9d8931
Benedict <jokanolaadedipupo@gmail.com>
2021-08-17 16:07:15 +0100

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,27 @@
 import React from 'react'
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar,Nav, Container } from "react-bootstrap";
+import logo from './logo.jpeg'
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src="/logo.svg"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-      React Bootstrap
-      </Navbar.Brand>
-    </Container>
-  </Navbar>
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Portfolio
+                    </Navbar.Brand>
+                    <Nav className="ms-auto">
+                    <Nav.Link href='#href'>Home</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </header>
     )
 }

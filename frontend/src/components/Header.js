364a79630942fe6175f7bbd9c1bb2ce78e6cf8a5
Benedict <jokanolaadedipupo@gmail.com>
2021-08-17 20:00:30 +0100

    contact

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import logo from './logo.jpeg'
 const Header = () => {
     return (
         <header className="mb-0">
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="#0000" variant="light">
                 <Container>
                     <Navbar.Brand href="#home">
                         <img
@@ -15,7 +15,7 @@ const Header = () => {
                             height="30"
                             className="d-inline-block align-top"
                         />{' '}
-                        Portfolio
+                        MyResume
                     </Navbar.Brand>
                     <Nav className="mr-auto" style={{color: 'white'}}>
                     <Nav.Link href='#href'>Home</Nav.Link>

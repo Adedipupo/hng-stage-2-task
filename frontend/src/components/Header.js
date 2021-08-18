af1e605bb5ba8b42b25d09738bd1440072ba7e68
Benedict <jokanolaadedipupo@gmail.com>
2021-08-18 08:50:11 +0100

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,37 @@
 import React from 'react'
-import { Navbar,Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from './logo.jpeg'
 
 const Header = () => {
     return (
         <header className="mb-0">
-            <Navbar bg="#0000" variant="light">
+            <Navbar collapseOnSelect bg="light" variant="light">
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img
-                            alt=""
-                            src={logo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />{' '}
-                        MyZuriCV
+                        <Navbar.Brand href="#home">
+                            <img
+                                alt=""
+                                src={logo}
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}
+                            MyZuriCV
+                        </Navbar.Brand>
                     </Navbar.Brand>
-                    <Nav className="mr-auto" style={{color: 'white'}}>
-                    <Nav.Link href='#href'>Home</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features"></Nav.Link>
+                            <Nav.Link href="#pricing"></Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Home</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>

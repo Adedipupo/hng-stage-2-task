29236327d09b9e91f108c6de0ab29df3520ccd67
Benedict <jokanolaadedipupo@gmail.com>
2021-08-18 13:11:52 +0100

    ready for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const dotenv = require('dotenv');
 const indexRouter = require('./routes/index.js');
 const { connectDB } = require('./config/db.js');
@@ -13,8 +14,17 @@ app.use(express.json())
 const port = process.env.PORT || 5000;
 
 
-
 app.use('/api/v1', indexRouter);
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => { res.send('API is running') })
+}
 
 app.get('/', (_req, res) => {
   res.redirect('/api/v1');
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Hompage from './pages/Hompage';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <>
-     <Hompage/>
-     <ToastContainer />   
-    </>
+    <Router>
+      <main>
+          <Route path="/" component={Hompage} exact />
+          <ToastContainer />
+      </main>
+
+    </Router>
   );
 }
 

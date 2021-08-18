8a63e138d16af308891a25039617d0be3940f277
Benedict <jokanolaadedipupo@gmail.com>
2021-08-18 08:51:53 +0100

    contact

--- frontend/src/components/contact.js
+++ frontend/src/components/contact.js
@@ -10,7 +10,7 @@ import FormContainer from "./FormContainer.js";
 const Contact = ({ location, history }) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [comments, setComments] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [message, setMessage] = useState(null);
 
